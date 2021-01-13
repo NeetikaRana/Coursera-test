8c87255f32a5422f3468640628f68f05ba73b338
Neetika.Rana <Neetika.Rana@ril.com>
2021-01-14 02:36:22 +0530

    mod5-1

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -103,8 +103,8 @@ function buildAndShowHomeHTML (categories) {
       console.log(chosen, typeof(chosen))
       // var chosenCategoryShortName = chosen. homeHtml
       var html = homeHtmlUrl;
-      var name = chosen[i].name;
-      var chosenCategoryShortName = chosen[i].short_name;
+      var name = chosen.name;
+      var chosenCategoryShortName = chosen.short_name;
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
